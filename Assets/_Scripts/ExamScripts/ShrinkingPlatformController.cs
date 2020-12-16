b29340b3961789f27229dca5fc6d115de0286a33
kossam001 <kossam001@gmail.com>
2020-12-16 16:34:52 -0500

    Platform shrinks over time

--- Assets/_Scripts/ExamScripts/ShrinkingPlatformController.cs
+++ Assets/_Scripts/ExamScripts/ShrinkingPlatformController.cs
@@ -1,18 +1,52 @@
-﻿using System.Collections;
+﻿/* ShrinkingPlatformController.cs
+ * 
+ * Name: Samuel Ko
+ * ID: 101168049
+ * Last Modified: 2020-12-16
+ * 
+ * A shrinking platform where if the player stands on it, it shrinks, otherwise it grows back to original size.
+ * 
+ * 2020-12-16: Added this script.
+ */ 
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ShrinkingPlatformController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float maxWidth;
+    public float maxSizeChangeDuration = 2;
+    public bool isShrinking = false;
+
+    private float width;
+    private float timer = 0;
+
+    private void Start()
     {
-        
+        maxWidth = width = transform.localScale.x;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionStay2D(Collision2D other)
     {
-        
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Shrink();
+        }
+    }
+
+    void Shrink()
+    {
+        if (width >= 0)
+        {
+            width = Mathf.Lerp(maxWidth, 0.0f, timer / maxSizeChangeDuration);
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            width = 0;
+        }
+
+        transform.localScale = new Vector3(width, transform.localScale.y);
     }
 }
